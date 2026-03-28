b161d74a0a0b431478107ababc7acedad89605df
ricardoruizus <ricardoruizusse@gmail.com>
2026-03-28 09:34:26 -0600

    Corrección url del Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Minijuegos/',  // ← nombre exacto de tu repo en GitHub
+  base: '/MiniJuegos/',  // ← nombre exacto de tu repo en GitHub
 })
